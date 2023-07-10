abf74b487240a99a0127fc6fe8d9b71ee954cc26
Meaghan Campbell <emeaghancampbell@gmail.com>
2023-07-10 19:51:30 -0400

    troubleshoot

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -12,9 +11,7 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000
-  },
-  devServer: {
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
